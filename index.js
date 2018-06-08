ac39af292fddd42b9ba499f14b5da93096472444
Landen Danyluk <landendanyluk@gmail.com>
2018-06-08 00:57:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function nestedTarget() {
 
 function increaseRankBy(n) {
   var items = document.querySelectorAll('ul.ranked-list li')
-  for (i = 0; i < items.length; i++) {
+  for (var i = 0; i < items.length; i++) {
     items[i].innerHTML = parseInt(items[i].innerHTML) + n
   }
 }
