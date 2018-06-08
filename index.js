0b87b7e447b3f218b2b27660b6010a0345146049
Landen Danyluk <landendanyluk@gmail.com>
2018-06-08 00:57:56 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+function getFirstSelector(selector) {
+  var result = document.querySelector(selector);
+  return result;
+}
+
+function nestedTarget() {
+  var result = document.getElementById('nested').getElementsByClassName('target')[0];
+  return result;
+}
+
+function increaseRankBy(n) {
+  var items = document.querySelectorAll('ul.ranked-list li')
+  for (var i = 0; i < items.length; i++) {
+    items[i].innerHTML = parseInt(items[i].innerHTML) + n
+  }
+}
+
+function heresAFuckingCondition(child) {
+  if (child.children.length == 0) {
+    return true
+  } else {
+    return false
+  }
+}
+
+
+function deepestChild(cond) {
+  var current = document.querySelector('div#grand-node')
+  while (current) {
+    if (heresAFuckingCondition(current)) {
+      return current
+    } else {
+      current = current.children[0]
+    }
+  }
+}
